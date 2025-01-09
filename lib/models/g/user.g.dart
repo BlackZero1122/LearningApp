d295963ed35f193eda65461cfc3d1351515ea902
BlackZero1122 <muhammad.tayyab@zilon.com>
2025-01-09 09:20:55 +0500

    ....

--- lib/models/g/user.g.dart
+++ lib/models/g/user.g.dart
@@ -17,109 +17,61 @@ class UserAdapter extends TypeAdapter<User> {
       for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
     };
     return User(
-      isFingerPrint: fields[0] as bool?,
-      isFingerPrintBi: fields[1] as bool?,
-      isUserCard: fields[2] as bool?,
-      isQrCode: fields[3] as bool?,
-      isUserPin: fields[4] as bool?,
-      photo: fields[5] as String?,
-      userId: fields[6] as String?,
-      displayName: fields[7] as String?,
-      firstName: fields[8] as String?,
-      lastName: fields[9] as String?,
-      email: fields[10] as String?,
-      username: fields[11] as String?,
-      password: fields[12] as String?,
-      companyName: fields[13] as String?,
-      method: fields[14] as String?,
-      plan: fields[15] as String?,
-      role: (fields[16] as List?)?.cast<String>(),
-      street: fields[17] as String?,
-      city: fields[18] as String?,
-      state: fields[19] as String?,
-      country: fields[20] as String?,
-      postalCode: fields[21] as String?,
-      phone: fields[22] as String?,
-      businessPhone: (fields[23] as List?)?.cast<String>(),
-      officeLocation: fields[24] as String?,
-      parentId: fields[25] as String?,
-      userLocations: (fields[26] as List?)?.cast<String>(),
-      userRights: (fields[28] as List?)?.cast<String>(),
-      type: fields[29] as String?,
-      age: fields[30] as num?,
-      gender: fields[31] as String?,
-      isBusinessSetup: fields[32] as bool?,
+      photo: fields[0] as String?,
+      userId: fields[1] as String?,
+      displayName: fields[2] as String?,
+      firstName: fields[3] as String?,
+      lastName: fields[4] as String?,
+      email: fields[5] as String?,
+      username: fields[6] as String?,
+      password: fields[7] as String?,
+      street: fields[8] as String?,
+      city: fields[9] as String?,
+      state: fields[10] as String?,
+      country: fields[11] as String?,
+      postalCode: fields[12] as String?,
+      phone: fields[13] as String?,
+      age: fields[14] as num?,
+      gender: fields[15] as String?,
     );
   }
 
   @override
   void write(BinaryWriter writer, User obj) {
     writer
-      ..writeByte(32)
-      ..writeByte(0)
-      ..write(obj.isFingerPrint)
-      ..writeByte(1)
-      ..write(obj.isFingerPrintBi)
-      ..writeByte(2)
-      ..write(obj.isUserCard)
-      ..writeByte(3)
-      ..write(obj.isQrCode)
-      ..writeByte(4)
-      ..write(obj.isUserPin)
-      ..writeByte(5)
-      ..write(obj.photo)
-      ..writeByte(6)
-      ..write(obj.userId)
-      ..writeByte(7)
-      ..write(obj.displayName)
-      ..writeByte(8)
-      ..write(obj.firstName)
-      ..writeByte(9)
-      ..write(obj.lastName)
-      ..writeByte(10)
-      ..write(obj.email)
-      ..writeByte(11)
-      ..write(obj.username)
-      ..writeByte(12)
-      ..write(obj.password)
-      ..writeByte(13)
-      ..write(obj.companyName)
-      ..writeByte(14)
-      ..write(obj.method)
-      ..writeByte(15)
-      ..write(obj.plan)
       ..writeByte(16)
-      ..write(obj.role)
-      ..writeByte(17)
+      ..writeByte(0)
+      ..write(obj.photo)
+      ..writeByte(1)
+      ..write(obj.userId)
+      ..writeByte(2)
+      ..write(obj.displayName)
+      ..writeByte(3)
+      ..write(obj.firstName)
+      ..writeByte(4)
+      ..write(obj.lastName)
+      ..writeByte(5)
+      ..write(obj.email)
+      ..writeByte(6)
+      ..write(obj.username)
+      ..writeByte(7)
+      ..write(obj.password)
+      ..writeByte(8)
       ..write(obj.street)
-      ..writeByte(18)
+      ..writeByte(9)
       ..write(obj.city)
-      ..writeByte(19)
+      ..writeByte(10)
       ..write(obj.state)
-      ..writeByte(20)
+      ..writeByte(11)
       ..write(obj.country)
-      ..writeByte(21)
+      ..writeByte(12)
       ..write(obj.postalCode)
-      ..writeByte(22)
+      ..writeByte(13)
       ..write(obj.phone)
-      ..writeByte(23)
-      ..write(obj.businessPhone)
-      ..writeByte(24)
-      ..write(obj.officeLocation)
-      ..writeByte(25)
-      ..write(obj.parentId)
-      ..writeByte(26)
-      ..write(obj.userLocations)
-      ..writeByte(28)
-      ..write(obj.userRights)
-      ..writeByte(29)
-      ..write(obj.type)
-      ..writeByte(30)
+      ..writeByte(14)
       ..write(obj.age)
-      ..writeByte(31)
-      ..write(obj.gender)
-      ..writeByte(32)
-      ..write(obj.isBusinessSetup);
+      ..writeByte(15)
+      ..write(obj.gender);
   }
 
   @override
--- lib/models/hive_models/user.dart
+++ lib/models/hive_models/user.dart
@@ -6,79 +6,41 @@ part '../g/user.g.dart';
 @HiveType(typeId: 3)
 class User extends IHiveBaseModel<User> {
   @HiveField(0)
-  bool? isFingerPrint;
-  @HiveField(1)
-  bool? isFingerPrintBi;
-  @HiveField(2)
-  bool? isUserCard;
-  @HiveField(3)
-  bool? isQrCode;
-  @HiveField(4)
-  bool? isUserPin;
-  @HiveField(5)
   String? photo;
-  @HiveField(6)
+  @HiveField(1)
   String? userId;
-  @HiveField(7)
+  @HiveField(2)
   String? displayName;
-  @HiveField(8)
+  @HiveField(3)
   String? firstName;
-  @HiveField(9)
+  @HiveField(4)
   String? lastName;
-  @HiveField(10)
+  @HiveField(5)
   String? email;
-  @HiveField(11)
+  @HiveField(6)
   String? username;
-  @HiveField(12)
+  @HiveField(7)
   String? password;
-  @HiveField(13)
-  String? companyName;
-  @HiveField(14)
-  String? method;
-  @HiveField(15)
-  String? plan;
-  @HiveField(16)
-  List<String>? role;
-  @HiveField(17)
+  @HiveField(8)
   String? street;
-  @HiveField(18)
+  @HiveField(9)
   String? city;
-  @HiveField(19)
+  @HiveField(10)
   String? state;
-  @HiveField(20)
+  @HiveField(11)
   String? country;
-  @HiveField(21)
+  @HiveField(12)
   String? postalCode;
-  @HiveField(22)
+  @HiveField(13)
   String? phone;
-  @HiveField(23)
-  List<String>? businessPhone;
-  @HiveField(24)
-  String? officeLocation;
-  @HiveField(25)
-  String? parentId;
-  @HiveField(26)
-  List<String>? userLocations;
-  @HiveField(28)
-  List<String>? userRights;
-  @HiveField(29)
-  String? type;
-  @HiveField(30)
+  @HiveField(14)
   num? age;
-  @HiveField(31)
+  @HiveField(15)
   String? gender;
-  @HiveField(32)
-  bool? isBusinessSetup;
 
   dynamic keyId;
-  String roles;
 
-  User({ this.roles="N/A",
-    this.isFingerPrint,
-    this.isFingerPrintBi,
-    this.isUserCard,
-    this.isQrCode,
-    this.isUserPin,
+  User({
     this.photo,
     this.userId,
     this.displayName,
@@ -87,36 +49,20 @@ class User extends IHiveBaseModel<User> {
     this.email,
     this.username,
     this.password,
-    this.companyName,
-    this.method,
-    this.plan,
-    this.role,
     this.street,
     this.city,
     this.state,
     this.country,
     this.postalCode,
     this.phone,
-    this.businessPhone,
-    this.officeLocation,
-    this.parentId,
-    this.userLocations,
-    this.userRights,
-    this.type,
     this.age,
     this.gender,
-    this.isBusinessSetup,
   });
 
   @override
   User fromJson(Map<String, dynamic> json) => User.fromJson(json);
 
   factory User.fromJson(Map<String, dynamic> json) => User(
-        isFingerPrint: json["isFingerPrint"],
-        isFingerPrintBi: json["isFingerPrintBI"],
-        isUserCard: json["isUserCard"],
-        isQrCode: json["isQRCode"],
-        isUserPin: json["isUserPin"],
         photo: json["photo"],
         userId: json["userID"],
         displayName: json["displayName"],
@@ -125,34 +71,18 @@ class User extends IHiveBaseModel<User> {
         email: json["email"],
         username: json["username"],
         password: json["password"],
-        companyName: json["companyName"],
-        method: json["method"],
-        plan: json["plan"],
-        role: List<String>.from(json["role"].map((x) => x)),
         street: json["street"],
         city: json["city"],
         state: json["state"],
         country: json["country"],
         postalCode: json["postalCode"],
         phone: json["phone"],
-        businessPhone: List<String>.from(json["businessPhone"].map((x) => x)),
-        officeLocation: json["officeLocation"],
-        parentId: json["parentID"],
-        userLocations: List<String>.from(json["userLocations"].map((x) => x)),
-        userRights: List<String>.from(json["userRights"].map((x) => x)),
-        type: json["type"],
         age: json["age"],
         gender: json["gender"],
-        isBusinessSetup: json["isBusinessSetup"],
       );
 
   @override
   Map<String, dynamic> toJson() => {
-        "isFingerPrint": isFingerPrint,
-        "isFingerPrintBI": isFingerPrintBi,
-        "isUserCard": isUserCard,
-        "isQRCode": isQrCode,
-        "isUserPin": isUserPin,
         "photo": photo,
         "userID": userId,
         "displayName": displayName,
@@ -161,24 +91,13 @@ class User extends IHiveBaseModel<User> {
         "email": email,
         "username": username,
         "password": password,
-        "companyName": companyName,
-        "method": method,
-        "plan": plan,
-        "role": List<dynamic>.from(role!.map((x) => x)),
         "street": street,
         "city": city,
         "state": state,
         "country": country,
         "postalCode": postalCode,
         "phone": phone,
-        "businessPhone": List<dynamic>.from(businessPhone!.map((x) => x)),
-        "officeLocation": officeLocation,
-        "parentID": parentId,
-        "userLocations": List<dynamic>.from(userLocations!.map((x) => x)),
-        "userRights": List<dynamic>.from(userRights!.map((x) => x)),
-        "type": type,
         "age": age,
         "gender": gender,
-        "isBusinessSetup": isBusinessSetup,
       };
 }
